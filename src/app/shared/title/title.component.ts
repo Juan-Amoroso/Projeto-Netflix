c36a74e3da47d85012125283638e314292d8f46e
Juan <juandluckaamoroso@gmail.com>
2022-03-07 21:05:40 -0300

    Finalizando

--- src/app/shared/title/title.component.ts
+++ src/app/shared/title/title.component.ts
@@ -1,4 +1,4 @@
-import { analyzeAndValidateNgModules } from '@angular/compiler';
+
 import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 
 @Component({
@@ -10,11 +10,11 @@ export class TitleComponent implements OnInit {
 
   
   @Input() title: any;
-  @ViewChild('trailer', { static: true }) trailer: ElementRef<HTMLVideoElement>;
+  @ViewChild('trailer', { static: true }) trailer: ElementRef<HTMLVideoElement> = {} as ElementRef
 
   constructor() {
 
-   
+  
 
    }
 
@@ -25,13 +25,30 @@ export class TitleComponent implements OnInit {
 
   playVideo() {
 
+   
     this.trailer.nativeElement.play();
 
   }
 
   stopVideo() {
 
+    this.trailer.nativeElement.currentTime = 0;
+    this.trailer.nativeElement.pause();
+
 
   }
+
+  getMuted(): boolean {
+
+    return this.trailer.nativeElement.muted;
+
+  }
+
+  toggleSound() {
+
+    this.trailer.nativeElement.muted = !this.trailer.nativeElement.muted;
+  }
+  
+  
 
 }
